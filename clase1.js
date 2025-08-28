78afb98a2542bdcd872b69c9e7bb0d0207dcd1e3
jsz-ar <jszarfmeser@itba.edu.ar>
2025-08-28 20:02:57 -0300

    tercer commit

--- clase1.js
+++ clase1.js
@@ -1,13 +1,13 @@
-let nombre = 'Esteban'; // String
-let edad = 32; // Number
+let nombre = 'Alfonso'; // String
+let edad = 99; // Number
 let pi = 3.14; // Number
-let profesor = true; // Boolean
+let humano = true; // Boolean
 const EULER = 2.71828; // Constante (no se puede modificar nunca)
 let camelCase = 'Funciona de esta manera'; // En vez de usar guiones bajos, usamos (a partir de la segunda palabra), mayusculas para cada incio de palabra
 // let tiene un scope local (si se crea en la funcion, no se puede usar afuera)
 // var tiene un scope global (se puede usar en cualquier lado, siendo creada en cualquier lugar)
 const NUMEROS = [1, 2, 3, 4]; // Mientras no se cambie el array a variable, push (append) no va a modificar la lista
-console.log();
+console.log(pi);
 
 const NACIONALIDADES = { // Objeto // Array de objetos literales // CRUD
 
@@ -18,3 +18,5 @@ const NACIONALIDADES = { // Objeto // Array de objetos literales // CRUD
 // TypeCasting
 // number para convertir en numero
 // string para convertir a string
+
+
