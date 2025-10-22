097e86ba5d100357dd95140edc64927cc314a43b
jsz-ar <jszarfmeser@itba.edu.ar>
2025-10-21 21:30:32 -0300

    agregarProducto Lista

--- script.js
+++ script.js
@@ -21,3 +21,32 @@ let carrito = [];
 
 const abrirCarrito  = () => carritoPanel.classList.add("abierto");
 const cerrarCarrito = () => carritoPanel.classList.remove("abierto");
+
+
+function agregarProducto(idBoton) {
+
+    if (!productos[idBoton]) {
+        return
+    }
+
+    let coincide = false;
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].id == idBoton) {
+            carrito[i].cantidad = carrito[i].cantidad + 1
+            coincide = true
+        break
+        }
+    }
+
+    if (!coincide) {
+        carrito.push({
+            id: idBoton,
+            nombre: productos[idBoton].nombre,
+            precio: productos[idBoton].precio,
+            cantidad: 1
+        });
+    }
+
+    // Agregar lo que sería obtenerProductos cuando termine con esa
+    abrirCarrito() // La idea sería que el carrito se abra cuando uno agrega un producto
+}
